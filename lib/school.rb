6c4f326e7bb4982ebd2c922efb6d5ed662f94a61
Erik Huang <erikhuangdrums@gmail.com>
2021-01-14 23:16:16 -0800

    done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,25 @@
-# code here!
+class School
+    attr_reader :model, :grade
+    attr_accessor :roster, :sort
+
+    def initialize(model)
+        @model = model
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ? @roster[grade] << name : @roster[grade] = [name]
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+
+    def sort
+        sorted_roster = @roster
+        sorted_roster.each do |k, v|
+            v.sort!
+        end
+        sorted_roster
+    end
+end
